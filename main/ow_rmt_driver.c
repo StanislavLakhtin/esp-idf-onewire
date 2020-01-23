b4850072894fc5821e4de95dfab3c753aba3dfa9
Stanislav Lakhtin <savant@savant.pro>
2020-01-23 11:40:40 +0300

    RMT TX Durations

--- main/ow_rmt_driver.c
+++ main/ow_rmt_driver.c
@@ -12,21 +12,32 @@ void ow_rmt_driver_init() {
   ESP_ERROR_CHECK( rmt_config( &_config_tx ));
   ESP_ERROR_CHECK(
       rmt_driver_install( _config_tx.channel, 0,
-                          ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED ));
+                          ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_SHARED ));
   rmt_config_t _config_rx = RMT_DEFAULT_CONFIG_RX( OW_RMT_RX_PIN, OW_RMT_RX_CHANNEL );
   _config_rx.rx_config.filter_en = false;
   _config_rx.clk_div = CLK_DIV;
   _config_rx.rx_config.idle_threshold = _IDLE_DURATION;
   ESP_ERROR_CHECK( rmt_config( &_config_rx ));
   ESP_ERROR_CHECK(
-      rmt_driver_install( _config_rx.channel, OW_RMT_RX_BUFFER_SIZE,
-                          ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED ));
+      rmt_driver_install( _config_rx.channel, ( sizeof(rmt_item32_t) + 8 ) * OW_RMT_RX_BUFFER_SIZE, // из-за RINGBUF_TYPE_NOSPLIT в RingbufferType_t
+                          ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_SHARED ));
 }
 
-static RingbufHandle_t * get_rx_buffer() {
-  RingbufHandle_t * _rb = NULL;
-  ESP_ERROR_CHECK( rmt_get_ringbuf_handle( OW_RMT_RX_CHANNEL, _rb ));
+static RingbufHandle_t get_rx_buffer() {
+  RingbufHandle_t _rb = NULL;
+  ESP_ERROR_CHECK( rmt_get_ringbuf_handle( OW_RMT_RX_CHANNEL, &_rb ));
   return _rb;
+}
+
+// flush any pending/spurious traces from the RX channel
+static void onewire_flush_rmt_rx_buf() {
+  void *p;
+  size_t s;
+  RingbufHandle_t * _rb = get_rx_buffer();
+
+  while ((p = xRingbufferReceive(_rb, &s, 0))) {
+    vRingbufferReturnItem(_rb, p);
+  }
 }
 
 uint16_t ow_rmt_reset( void ) {
@@ -36,13 +47,15 @@ uint16_t ow_rmt_reset( void ) {
   ESP_ERROR_CHECK(
       rmt_set_rx_idle_thresh( OW_RMT_RX_CHANNEL, OW_MS_TO_RMT_TICKS( _RESET_DURATION + _RESET_IDLE_DURATION )));
   // инициируем цикл чтения с очисткой кольцевого буфера чтения
-  ESP_ERROR_CHECK( rmt_rx_start( OW_RMT_RX_CHANNEL, true ));
-  // обычное состояние шины -- подтянуто к питанию. Создаём импульс сброса нужной длинны
-  rmt_item32_t reset_ticks = OW_TICK_SLOT_MS_LOW_HIGH( _RESET_DURATION, 0 );
+  // todo check onewire_flush_rmt_rx_buf();
+  //ESP_ERROR_CHECK( rmt_rx_start( OW_RMT_RX_CHANNEL, true ));
+  // обычное состояние шины -- подтянуто к питанию. Создаём импульс, подтягивая к земле, нужной длинны
+  rmt_item32_t reset_ticks = OW_TICK_SLOT_MS_LOW( _RESET_DURATION );
+  ESP_LOGI("OW", "RESET duration: %d ticks ( %d ms, CLK_DIV = %d)", reset_ticks.duration0, _RESET_DURATION, CLK_DIV);
   if ( rmt_write_items( OW_RMT_TX_CHANNEL, &reset_ticks, 1, true ) == ESP_OK ) {
-    RingbufHandle_t * _rb = get_rx_buffer();
+    /*RingbufHandle_t _rb = get_rx_buffer();
     uint32_t length;
-    rmt_item32_t * _items = ( rmt_item32_t * ) xRingbufferReceive( _rb, &length, 1 / portTICK_PERIOD_MS);
+    rmt_item32_t * _items = ( rmt_item32_t * ) xRingbufferReceive( _rb, &length, 10 / portTICK_PERIOD_MS);
     if ( _items ) {
       // мы что-то получили. Теперь следует проверить, что прилетело
       // каждый слот представляет собой около 8ms.
@@ -57,8 +70,8 @@ uint16_t ow_rmt_reset( void ) {
       }
       // после разбора данных, сбрасываем ringbuffer.
       vRingbufferReturnItem( _rb, ( void * ) _items );
-    }
+    }*/
   }
-  ESP_ERROR_CHECK( rmt_rx_stop( OW_RMT_RX_CHANNEL ));
+  //ESP_ERROR_CHECK( rmt_rx_stop( OW_RMT_RX_CHANNEL ));
   return _presence;  // вернём ненулевое значение в виде длительности PRESENCE в тиках
 }
--- main/ow_rmt_driver.h
+++ main/ow_rmt_driver.h
@@ -18,39 +18,27 @@
 #define OW_RMT_TX_CHANNEL       RMT_CHANNEL_0
 #define OW_RMT_RX_CHANNEL       RMT_CHANNEL_1
 
-#define OW_RMT_RX_BUFFER_SIZE   8
+#define OW_RMT_RX_BUFFER_SIZE   28          // elements (CLK_DIV = 200, 70ms = 28 elements)
 
 #define APB_CLK                 80000000    // todo пересчитать все tick от реального значения APB_CLK
-#define CLK_DIV                 10
+#define CLK_DIV                 240
 
+#define _RMT_OVERDRIVE          2
 #define _RESET_DURATION         480
 #define _RESET_IDLE_DURATION    70
 #define _IDLE_DURATION          70
 
-#define OW_MS_TO_RMT_TICKS( ms )  ( ms / 80 * CLK_DIV)
+#define OW_MS_TO_RMT_TICKS( ms )  ( ( ( ms ) * 10 ) / ( CLK_DIV / 8 )  )
 
-#define OW_TICK_SLOT_MS_LOW_HIGH( low_duration, high_duration )   \
+#define OW_TICK_SLOT_MS_LOW( low_duration_ms )   \
     {                                                             \
         {                                                         \
-            .duration0 = OW_MS_TO_RMT_TICKS(low_duration),        \
+            .duration0 = OW_MS_TO_RMT_TICKS(low_duration_ms),     \
             .level0 = 0,                                          \
-            .duration1 = OW_MS_TO_RMT_TICKS(high_duration),       \
+            .duration1 = 0,                                       \
             .level1 = 1                                           \
         }                                                         \
     }
-
-/*
- * 1ms это слишком мало [как строительная единица] для формирования нормального сигнала 1-wire.
- * Более менее нормальная единица измерения это 8ms. Руководствуясь этим соображением, размер слота
- * для чтения составляет 8ms .
- * Типовая частота APB_CLK = 80 000 000 Hz
- * 1 ms достигается при стандартном делителе в 80
- * соответственно, 8 ms мы можем достичь с делителем в 10
- * Чтобы обеспечить нужное количество tick, делитель (clk_div) на RX канале мы определяем
- * в 10 (_config_rx.clk_div = 10;)
- * размер Tick : 1 сек / ( 80 000 000 / 10 )
- * Макрос OW_TICK_SLOT_MS_LOW_HIGH нужен, чтобы удобно определять структуру в одну строчку, делая код плотнее
- * */
 
 #ifdef __cplusplus
 extern "C"
